e0e5afe0ee07e653ecad575c699d15450903dc8c
TWAM-NARUTO-POWER <98334586+TWAM-NARUTO-POWER@users.noreply.github.com>
2022-01-24 21:19:13 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -35,6 +35,74 @@ HELP = """
 ABOUT = """
 ➥ Name : Auto Filter With IMDB
     
+➥ Credits : NARUTO
+
+➥ Editing : [NARUTO SECOND](https://t.me/CALL_ME_NARUTO_TEAM)
+
+➥ Language : Python3
+
+➥ Library : Pyrogram Asyncio 1.13.0
+
+➥ Source Code : [Click Me](from .Utils import (
+
+   get_filter_results,
+
+   get_file_details,
+
+   is_subscribed,
+
+   get_poster,
+
+   Media
+
+)
+
+from .Channel import (
+
+   RATING,
+
+   GENRES
+
+)
+
+HELP = """
+
+<b><u>Basic Commads</u></b>
+
+➥ /start : check if am alive or dead
+
+➥ /about : About Me
+
+➥ /stats user Status
+
+<b><u>Bot Owner Only</u></b>
+
+➥ /broadcast Replay Any Message or Media
+
+➥ /ban_user  click ban_user more info
+
+➥ /unban_user click unban_user more info
+
+➥ /banned_users Banned User Details
+
+➥ /total How Many Files Added In Database
+
+➥ /logger  Get Logs
+
+➥ /delete Delete File From Database
+
+<b>[➡️How To Add Old Files For database](https://youtu.be/mtu5hgW-l_o)
+
+@Mo_Tech_YT></b>
+
+"""
+
+ABOUT = """
+
+➥ Name : Auto Filter With IMDB
+
+    
+
 ➥ Credits : Subin
 
 ➥ Editing : [Muhammed RK](t.me/Mrk_YT)
@@ -44,4 +112,6 @@ ABOUT = """
 ➥ Library : Pyrogram Asyncio 1.13.0
 
 ➥ Source Code : [Click Me](https://github.com/PR0FESS0R-99/LuciferMoringstar_Robot)
+
+""")
 """
